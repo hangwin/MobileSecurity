10d03a5eae9cdd3525a282dfeabb9ca19f5870cd
hangwin <yzhll123@163.com>
2016-03-31 09:10:36 +0800

    短信备份添加进度条，增加短信恢复功能

--- app/src/main/java/com/study/hang/mobileSecurity/activity/AdvancedToolActivity.java
+++ app/src/main/java/com/study/hang/mobileSecurity/activity/AdvancedToolActivity.java
@@ -2,6 +2,7 @@ package com.study.hang.mobileSecurity.activity;
 
 import android.annotation.TargetApi;
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.ContentResolver;
 import android.content.Intent;
 import android.database.Cursor;
@@ -13,15 +14,21 @@ import android.provider.Telephony;
 import android.util.Xml;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.study.hang.db.smsEntity;
 import com.study.hang.mobileSecurity.R;
 
 import org.w3c.dom.Text;
+import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlSerializer;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by hang on 16/3/26.
@@ -30,6 +37,9 @@ public class AdvancedToolActivity extends Activity {
     private TextView numberbelong;
     private TextView smsbackup;
     private TextView smsrecovery;
+    private ProgressDialog progressDialog;
+    private List<smsEntity> list;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,12 +57,25 @@ public class AdvancedToolActivity extends Activity {
         smsbackup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               new Thread(new Runnable() {
+                progressDialog=new ProgressDialog(AdvancedToolActivity.this);
+                progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+                progressDialog.setMessage("备份中...");
+                progressDialog.show();
+
+                new Thread(new Runnable() {
                    @Override
                    public void run() {
-                       backup();
+                       backup(progressDialog);
+                       runOnUiThread(new Runnable() {
+                           @Override
+                           public void run() {
+                               Toast.makeText(AdvancedToolActivity.this,"备份成功",Toast.LENGTH_SHORT).show();
+                               progressDialog.dismiss();
+                           }
+                       });
                    }
                }).start();
+
             }
         });
 
@@ -60,10 +83,23 @@ public class AdvancedToolActivity extends Activity {
         smsrecovery.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                progressDialog=new ProgressDialog(AdvancedToolActivity.this);
+                progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+                progressDialog.setMessage("恢复中...");
+                progressDialog.show();
                 new Thread(new Runnable() {
                     @Override
                     public void run() {
-                        recovery();
+                        System.out.println("before...");
+                        recovery(progressDialog);
+                        System.out.println("====》" + list);
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                progressDialog.dismiss();
+                               Toast.makeText(AdvancedToolActivity.this,"恢复成功",Toast.LENGTH_SHORT).show();
+                            }
+                        });
                     }
                 }).start();
             }
@@ -71,25 +107,74 @@ public class AdvancedToolActivity extends Activity {
     }
 
     @TargetApi(Build.VERSION_CODES.KITKAT)
-    private void recovery() {
+    private void recovery(ProgressDialog progressDialog) {
+        try {
+            XmlPullParser parser = Xml.newPullParser();
+            File file = new File(Environment.getExternalStorageDirectory().getAbsolutePath(), "sms.xml");
+            parser.setInput(new FileInputStream(file), "utf-8");
+            int eventType= parser.getEventType();
+            smsEntity sms=null;
+            int num=0;
+            while (eventType!=XmlPullParser.END_DOCUMENT) {
 
+                switch (eventType) {
+                    case XmlPullParser.START_TAG:
+                        if("smss".equals(parser.getName())) {
+                            progressDialog.setMax( Integer.parseInt(parser.getAttributeValue(null, "count")));
+                            list=new ArrayList<smsEntity>();
+                        }else if("sms".equals(parser.getName())) {
+                           sms=new smsEntity();
+                        }else if("address".equals(parser.getName())) {
+                            sms.setAddress(parser.nextText());
+                        }else if("date".equals(parser.getName())) {
+                            sms.setAddress(parser.nextText());
+                        }
+                        else if("type".equals(parser.getName())) {
+                            sms.setAddress(parser.nextText());
+                        }
+                        else if("body".equals(parser.getName())) {
+                            sms.setAddress(parser.nextText());
+                        }
 
+                        break;
+                    case XmlPullParser.END_TAG:
+                        if("sms".equals(parser.getName())) {
+                            list.add(sms);
+                        }
+                        eventType=parser.next();
+                        break;
+                }
+                progressDialog.setProgress(num++);
+            }
+
+        }catch (Exception e) {
+            e.printStackTrace();
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Toast.makeText(AdvancedToolActivity.this,"恢复失败",Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
     }
 
     @TargetApi(Build.VERSION_CODES.KITKAT)
-    private void backup() {
+    private void backup(ProgressDialog progressDialog) {
 
         try {
             ContentResolver resolver = getContentResolver();
-            Uri uri = Uri.parse("content://");
             File file = new File(Environment.getExternalStorageDirectory().getAbsolutePath(), "sms.xml");
             XmlSerializer serializer = Xml.newSerializer();
             serializer.setOutput(new FileOutputStream(file), "utf-8");
             serializer.startDocument("utf-8", true);
             serializer.startTag(null, "smss");
             Cursor cursor = resolver.query(Telephony.Sms.CONTENT_URI, new String[]{"address", "date", "type", "body"}, null, null, null);
-            serializer.attribute(null,"count",cursor.getColumnCount()+"");
+            serializer.attribute(null, "count", cursor.getColumnCount() + "");
+            progressDialog.setMax(cursor.getColumnCount());
+            int current=0;
             while (cursor.moveToNext()) {
+                Thread.sleep(1000);
+                progressDialog.setProgress(current++);
                 System.out.println("短信内容====》" + cursor.getString(3));
                 serializer.startTag(null,"sms");
                 serializer.startTag(null, "address");
@@ -112,6 +197,12 @@ public class AdvancedToolActivity extends Activity {
             serializer.endDocument();
         }catch (Exception e) {
             e.printStackTrace();
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Toast.makeText(AdvancedToolActivity.this,"备份失败",Toast.LENGTH_SHORT).show();
+                }
+            });
         }
     }
 }
